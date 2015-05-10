5a9cd8ffdebfd2e679dd2338b56db220be11f15a
nlogo <nlogo0816@gmail.com>
2015-05-10 21:45:54 +0800

    [CS252]2015051000
    FingerGuessing

---
+++ FingerGuessing/FingerGuessing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+
+                Console.WriteLine("選擇您要出的拳 1(剪刀) 2(石頭) 3(布) :");
+                string MyChoice = Console.ReadLine();
+
+                int PcChoice = random.Next(1, 3);
+
+                Console.WriteLine("你選擇{0}，電腦選擇{1}，餘數為{2}", MyChoice, PcChoice.ToString(), PcChoice - int.Parse(MyChoice));
+
+                if (PcChoice == int.Parse(MyChoice))
+                {
+                    Console.WriteLine("平手");
+                }
+                else if ((PcChoice - int.Parse(MyChoice)) >= -1 && (PcChoice - int.Parse(MyChoice)) != 1)
+                {
+                    Console.WriteLine("你贏了");
+                }
+                else
+                {
+                    Console.WriteLine("你輸了");
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}

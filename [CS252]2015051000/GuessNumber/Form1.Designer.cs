972cd5a221582a784acf0f40a68ce312ffa3e736
nlogo <nlogo0816@gmail.com>
2015-05-15 01:11:57 +0800

    [CS252]2015051000
    GuessNu,ber

---
+++ [CS252]2015051000/GuessNumber/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GuessNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GuessNumberBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GuessNumberBox
+            // 
+            this.GuessNumberBox.Location = new System.Drawing.Point(83, 71);
+            this.GuessNumberBox.Name = "GuessNumberBox";
+            this.GuessNumberBox.Size = new System.Drawing.Size(100, 22);
+            this.GuessNumberBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "要猜的數字";
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(14, 21);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 2;
+            this.ResetButton.Text = "重開新局";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Location = new System.Drawing.Point(197, 71);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(75, 23);
+            this.GuessButton.TabIndex = 3;
+            this.GuessButton.Text = "猜一猜";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 110);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 350);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 472);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.GuessButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GuessNumberBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "1A2B";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox GuessNumberBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button GuessButton;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ [CS252]2015051000/GuessNumber/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumber
+{
+    public partial class Form1 : Form
+    {
+
+        string MyGuessList;
+        string Number;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GuessButton_Click(object sender, EventArgs e)
+        {
+            string MyGuess = GuessNumberBox.Text;
+
+            MessageBox.Show(MyGuess + "  " + Number);
+            int a = 0, b = 0;
+
+            for (int i = 1; i <= 1000; i *= 10)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetNumber();
+        }
+
+        private void SetNumber() 
+        {
+            Random random = new Random();
+            int Num = random.Next(0, 9999);
+
+            if (Num < 10)
+            {
+                this.Number = "000" + Num.ToString();
+            }
+            else if (Num < 100)
+            {
+                this.Number = "00" + Num.ToString();
+            }
+            else if (Num < 1000)
+            {
+                this.Number = "0" + Num.ToString();
+            }
+            else 
+            {
+                this.Number = Num.ToString();
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            SetNumber();
+            MessageBox.Show(this.Number);
+        }
+                
+    }
+}
